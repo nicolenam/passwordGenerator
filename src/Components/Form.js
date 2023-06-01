2114ff247c425654b152bcaf34dc3ce0fa953f4a
nicole-nam <eunjungnam@gmail.com>
2023-06-01 12:57:42 -0400

    Fixed some css styles for the track and the thumb

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -7,7 +7,7 @@ const Form = () =>{
                 <h2>character length</h2>
                 <p>10</p>
             </div>
-            <input className="slider" type="range" name="" min="1" max="20" />
+            <input className="slider" type="range" min="1" max="20" />
             <div>
                 checkboxes for options
             </div>
